dc3e68f9f8916392269f5d3f67c12955510f648b
Kameswari Chamarthi <kameswarichamarthi683@gmail.com>
2025-04-05 22:47:16 +0530

    Your commit message

--- PXFF-main/js/main.js
+++ PXFF-main/js/main.js
@@ -121,7 +121,7 @@ async function detectDeepfake(file) {
     formData.append("image", file);  // "image" must match Flask API
 
     try {
-        let response = await fetch("http://127.0.0.1:5000/detect-deepfake", {
+        let response = await fetch("https://web-production-7d67.up.railway.app/detect-deepfake", { // <-- Updated URL
             method: "POST",
             body: formData
         });
@@ -143,6 +143,7 @@ async function detectDeepfake(file) {
         alert("⚠️ Deepfake detection failed. Please try again.");
     }
 }
+
 
 /*=============== REDIRECT TO RESULTS PAGE ===============*/
 function redirectToResultsPage() {
